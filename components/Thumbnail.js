eac6f1dccef1c635001ab3fc7380d2ce300ea198
FourSqweez <four7518@gmail.com>
2021-04-28 16:14:24 +0700

    Added : flipMove and forwardRef

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import { ThumbUpIcon } from '@heroicons/react/outline'
+import { forwardRef } from 'react'
 
-export default function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original'
   console.log(result)
   return (
     <div
+      ref={ref}
       className="p-2 group cursor-pointer transition duration-200
      ease-in transform sm:hover:scale-105 hover:z-50 xs:hover:scale-105 "
     >
@@ -34,4 +36,6 @@ export default function Thumbnail({ result }) {
       </div>
     </div>
   )
-}
+})
+
+export default Thumbnail
