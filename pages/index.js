d3d3754e54d99308d30b883315192db701061594
FourSqweez <four7518@gmail.com>
2021-04-28 12:50:37 +0700

    Added : HeaderItem

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hi test</h1>
+
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
     </div>
   )
 }
